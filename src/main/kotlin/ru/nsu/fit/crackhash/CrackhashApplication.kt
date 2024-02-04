9c0a95365b9ba77f48c8ad694ea03152d1fb3e0e
egorkuzn <egorkuzn2002@mail.ru>
2024-02-04 14:04:50 +0700

    Repo configure

---
+++ src/main/kotlin/ru/nsu/fit/crackhash/CrackhashApplication.kt
@@ -0,0 +1,11 @@
+package ru.nsu.fit.crackhash
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrackhashApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrackhashApplication>(*args)
+}
---
+++ src/test/kotlin/ru/nsu/fit/crackhash/CrackhashApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.nsu.fit.crackhash
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrackhashApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
