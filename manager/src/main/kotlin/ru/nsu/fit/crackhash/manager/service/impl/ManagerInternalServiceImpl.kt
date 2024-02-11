f1c57efab88df7c9e8beb8a5b627a8cf22e1c555
egorkuzn <egorkuzn2002@mail.ru>
2024-02-11 15:55:33 +0700

    Forget about  @OptIn(DelicateCoroutinesApi::class).

--- manager/src/main/kotlin/ru/nsu/fit/crackhash/manager/service/impl/ManagerInternalServiceImpl.kt
+++ manager/src/main/kotlin/ru/nsu/fit/crackhash/manager/service/impl/ManagerInternalServiceImpl.kt
@@ -1,5 +1,6 @@
 package ru.nsu.fit.crackhash.manager.service.impl
 
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.slf4j.Logger
@@ -15,6 +16,7 @@ class ManagerInternalServiceImpl(
     private val logger: Logger,
     private val responseService: ResponseService,
 ) : ManagerInternalService {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun crackRequest(response: WorkerResponseDto) {
         if (isTimeout(response)) {
             logger.info("Got result [${response.partNumber}|$partCount] of ${response.requestId} TIMEOUT")
